33328765462d7464f91fdf07a627f8dccae72745
Yehor Melnychuk <myegor15@gmail.com>
2026-08-02 00:11:08 +0300

    refactor

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module xyz.melnychuk.niimprint {
+
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.swing;
+
     requires java.desktop;
     requires java.logging;
     requires java.net.http;
@@ -12,13 +14,13 @@ module xyz.melnychuk.niimprint {
 
     requires static lombok;
 
-    opens xyz.melnychuk.niimprint to javafx.fxml;
     exports xyz.melnychuk.niimprint;
+
+    opens xyz.melnychuk.niimprint.controller to javafx.fxml;
 
     opens xyz.melnychuk.niimprint.model to com.fasterxml.jackson.databind;
     opens xyz.melnychuk.niimblue to com.fasterxml.jackson.databind;
     opens xyz.melnychuk.niimblue.request to com.fasterxml.jackson.databind;
     opens xyz.melnychuk.niimblue.response to com.fasterxml.jackson.databind;
-    exports xyz.melnychuk.niimprint.controller;
-    opens xyz.melnychuk.niimprint.controller to javafx.fxml;
+
 }
